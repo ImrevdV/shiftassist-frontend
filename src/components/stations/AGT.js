79ae43622cdca2a403a375552b448dab478185a7
ImrevdV <imre.vdvoort@gmail.com>
2022-06-23 18:16:21 +0200

    started station page styling

--- src/components/stations/AGT.js
+++ src/components/stations/AGT.js
@@ -3,10 +3,18 @@ import back from '../../assets/arrow-back.svg';
 import './Station.css';
 
 export default function AGT() {
+    function setValue(event) {
+        console.log(event.target.value);
+    }
     return <main className="Station-main">
         <nav className='navigation'>
             <a href='/home'><img src={back} className="back" alt="back arrow" /></a>
             <h1>AGT</h1>
         </nav>
+        <header className='header'>
+            <p>type</p> <p className='rightp'>amount</p>
+        </header>
+        <p><label for="row">ROW</label></p>
+        <input type="number" id="row" name="row" min="0" max="9999" step="1" value="0" onChange={setValue}></input>
     </main>;
 }

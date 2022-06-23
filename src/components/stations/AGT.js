2bc13f3881366028b692db3dddcba7e0f4b5d631
ImrevdV <imre.vdvoort@gmail.com>
2022-06-23 20:43:24 +0200

    station pagina styling + berekeningen

--- src/components/stations/AGT.js
+++ src/components/stations/AGT.js
@@ -2,19 +2,60 @@ import React from 'react';
 import back from '../../assets/arrow-back.svg';
 import './Station.css';
 
-export default function AGT() {
-    function setValue(event) {
-        console.log(event.target.value);
+class AGT extends React.Component {
+
+    state = {totaal: 0, sampling: 0, rc: 0, pallets: 0, retour: 0};
+
+    setValue = event => {
+        switch (event.target.name) {
+            case "sampling":
+                this.setState({sampling: event.target.value});
+                break;
+            case "rc":
+                this.setState({rc: event.target.value});
+                break;
+            case "pallets":
+                this.setState({pallets: event.target.value});
+                break;
+            case "retour":
+                this.setState({retour: event.target.value});
+                break;
+        }
+        this.setState({totaal: this.state.sampling * 10 + this.state.rc * 10 + this.state.retour * 10 + this.state.pallets * 11});
+        console.log(this.state.totaal);
     }
-    return <main className="Station-main">
-        <nav className='navigation'>
-            <a href='/home'><img src={back} className="back" alt="back arrow" /></a>
-            <h1>AGT</h1>
-        </nav>
-        <header className='header'>
-            <p>type</p> <p className='rightp'>amount</p>
-        </header>
-        <p><label for="row">ROW</label></p>
-        <input type="number" id="row" name="row" min="0" max="9999" step="1" value="0" onChange={setValue}></input>
-    </main>;
+
+    render() {
+        return (
+            <main className="Station-main">
+                <nav className='navigation'>
+                    <a href='/home'><img src={back} className="back" alt="back arrow" /></a>
+                    <h1>AGT</h1>
+                </nav>
+                <header className='header'>
+                    <p>type</p> <p className='rightp'>amount</p>
+                </header>
+                <section className='inputLine'>
+                    <label htmlFor="sampling">Sampling</label>
+                    <input type="number" id="sampling" name="sampling" min="0" max="999" placeholder="0" onChange={this.setValue}></input>
+                </section>
+                <section className='inputLine'>
+                    <label htmlFor="rc">RC's gericht</label>
+                    <input type="number" id="rc" name="rc" min="0" max="999" placeholder="0" onChange={this.setValue}></input>
+                </section>
+                <section className='inputLine'>
+                    <label htmlFor="pallets">Pallets gericht</label>
+                    <input type="number" id="pallets" name="pallets" min="0" max="999" placeholder="0" onChange={this.setValue}></input>
+                </section>
+                <section className='inputLine'>
+                    <label htmlFor="retour">Retouren UD</label>
+                    <input type="number" id="retour" name="retour" min="0" max="999" placeholder="0" onChange={this.setValue}></input>
+                </section>
+                <a className='submitButton' href='/home'><button className='Button'>Submit</button></a>
+            </main>
+        )
+        
+    }
 }
+    
+export default AGT;
--- src/components/stations/PKT.js
+++ src/components/stations/PKT.js
@@ -2,11 +2,60 @@ import React from 'react';
 import back from '../../assets/arrow-back.svg';
 import './Station.css';
 
-export default function PKT() {
-    return <main className="Station-main">
-        <nav className='navigation'>
-            <a href='/home'><img src={back} className="back" alt="back arrow" /></a>
-            <h1>PKT</h1>
-        </nav>
-    </main>;
+class PKT extends React.Component {
+
+    state = {totaal: 0, sampling: 0, rc: 0, pallets: 0, retour: 0};
+
+    setValue = event => {
+        switch (event.target.name) {
+            case "sampling":
+                this.setState({sampling: event.target.value});
+                break;
+            case "rc":
+                this.setState({rc: event.target.value});
+                break;
+            case "pallets":
+                this.setState({pallets: event.target.value});
+                break;
+            case "retour":
+                this.setState({retour: event.target.value});
+                break;
+        }
+        this.setState({totaal: this.state.sampling * 10 + this.state.rc * 10 + this.state.retour * 10 + this.state.pallets * 11});
+        console.log(this.state.totaal);
+    }
+
+    render() {
+        return (
+            <main className="Station-main">
+                <nav className='navigation'>
+                    <a href='/home'><img src={back} className="back" alt="back arrow" /></a>
+                    <h1>PKT</h1>
+                </nav>
+                <header className='header'>
+                    <p>type</p> <p className='rightp'>amount</p>
+                </header>
+                <section className='inputLine'>
+                    <label htmlFor="sampling">Sampling</label>
+                    <input type="number" id="sampling" name="sampling" min="0" max="999" placeholder="0" onChange={this.setValue}></input>
+                </section>
+                <section className='inputLine'>
+                    <label htmlFor="rc">RC's gericht</label>
+                    <input type="number" id="rc" name="rc" min="0" max="999" placeholder="0" onChange={this.setValue}></input>
+                </section>
+                <section className='inputLine'>
+                    <label htmlFor="pallets">Pallets gericht</label>
+                    <input type="number" id="pallets" name="pallets" min="0" max="999" placeholder="0" onChange={this.setValue}></input>
+                </section>
+                <section className='inputLine'>
+                    <label htmlFor="retour">Retouren UD</label>
+                    <input type="number" id="retour" name="retour" min="0" max="999" placeholder="0" onChange={this.setValue}></input>
+                </section>
+                <a className='submitButton' href='/home'><button className='Button'>Submit</button></a>
+            </main>
+        )
+        
+    }
 }
+    
+export default PKT;

49d845941f79940ba4e8535b4428d8856df7f145
ImrevdV <imre.vdvoort@gmail.com>
2022-06-23 17:36:08 +0200

    navbar station/currentflow

--- src/components/Flow.js
+++ src/components/Flow.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import back from '../assets/arrow-back.svg';
 import './Flow.css';
 
 export default function Flow() {
     return <main className="Flow-main">
-        <h1>flow</h1>
+        <nav className='navigation'>
+            <a href='/home'><img src={back} className="back" alt="back arrow" /></a>
+            <h1>Current Flow</h1>
+        </nav>
     </main>;
 }
--- src/components/stations/BOK.js
+++ src/components/stations/BOK.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import back from '../../assets/arrow-back.svg';
 import './Station.css';
 
 export default function BOK() {
     return <main className="Station-main">
-        <h1>BOK</h1>
+        <nav className='navigation'>
+            <a href='/home'><img src={back} className="back" alt="back arrow" /></a>
+            <h1>BOK</h1>
+        </nav>
     </main>;
 }

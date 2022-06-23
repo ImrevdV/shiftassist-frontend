42a6a8c6d74b5f421f272c6eb70c32f875ce60ff
ImrevdV <imre.vdvoort@gmail.com>
2022-06-23 20:56:46 +0200

    started flow page styling

--- src/components/Flow.js
+++ src/components/Flow.js
@@ -2,11 +2,23 @@ import React from 'react';
 import back from '../assets/arrow-back.svg';
 import './Flow.css';
 
-export default function Flow() {
-    return <main className="Flow-main">
-        <nav className='navigation'>
-            <a href='/home'><img src={back} className="back" alt="back arrow" /></a>
-            <h1>Current Flow</h1>
-        </nav>
-    </main>;
+class Flow extends React.Component {
+    render() {
+        return (
+            <main className="Flow-main">
+                <nav className='navigation'>
+                    <a href='/home'><img src={back} className="back" alt="back arrow" /></a>
+                    <h1>Current Flow</h1>
+                </nav>
+                <header className='header'>
+                    <p>station</p> <p className='rightp'>%</p>
+                </header>
+                <header className='header'>
+                    <p>suggested actions</p>
+                </header>
+            </main>
+        )
+    }
 }
+
+export default Flow;

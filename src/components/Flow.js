ca0ce35e72b566329a98fecf6d81a3205d3931b2
ImrevdV <imre.vdvoort@gmail.com>
2022-06-23 23:41:54 +0200

    flow page done

--- src/components/Flow.js
+++ src/components/Flow.js
@@ -4,8 +4,8 @@ import './Flow.css';
 
 class Flow extends React.Component {
 
-    state = { currentAGT: 90, currentBOK: 104, currentSPIN: 111, currentPKT: 95};
-    
+    state = { currentAGT: 90, currentBOK: 104, currentSPIN: 111, currentPKT: 95 };
+
     setGraphwidths() {
         document.documentElement.style.setProperty('--current-agt', '${this.state.currentAGT}%')
         document.documentElement.style.setProperty('--current-bok', '${this.state.currentAGT}%')
@@ -29,33 +29,48 @@ class Flow extends React.Component {
                             AGT
                             <div className='graph-bar-current current-agt'></div>
                         </div>
-                        <p>10⮟</p>
+                        {this.state.currentAGT > 100 ? <p>{this.state.currentAGT - 100}▲</p> : <p>{100 - this.state.currentAGT}▼</p>}
                     </section>
                     <section className='graph-section'>
                         <div className='graph-bar'>
                             BOK
                             <div className='graph-bar-current current-bok'></div>
                         </div>
-                        <p>10⮟</p>
+                        {this.state.currentBOK > 100 ? <p>{this.state.currentBOK - 100}▲</p> : <p>{100 - this.state.currentBOK}▼</p>}
                     </section>
                     <section className='graph-section'>
                         <div className='graph-bar'>
                             SPIN
                             <div className='graph-bar-current current-spin'></div>
                         </div>
-                        <p>10⮟</p>
+                        {this.state.currentSPIN > 100 ? <p>{this.state.currentSPIN - 100}▲</p> : <p>{100 - this.state.currentSPIN}▼</p>}
                     </section>
                     <section className='graph-section'>
                         <div className='graph-bar'>
                             PKT
                             <div className='graph-bar-current current-pkt'></div>
                         </div>
-                        <p>10⮟</p>
+                        {this.state.currentPKT > 100 ? <p>{this.state.currentPKT - 100}▲</p> : <p>{100 - this.state.currentPKT}▼</p>}
                     </section>
                 </section>
                 <header className='header'>
                     <p>suggested actions</p>
                 </header>
+                <section className='suggestedAction'>
+                    <section className='higher'>
+                        {this.state.currentAGT > 100 && <p>AGT</p>}
+                        {this.state.currentBOK > 100 && <p>BOK</p>}
+                        {this.state.currentSPIN > 100 && <p>SPIN</p>}
+                        {this.state.currentPKT > 100 && <p>PKT</p>}
+                    </section>
+                    <p>ᐅᐅᐅ</p>
+                    <section className='lower'>
+                        {this.state.currentAGT < 100 && <p>AGT</p>}
+                        {this.state.currentBOK < 100 && <p>BOK</p>}
+                        {this.state.currentSPIN < 100 && <p>SPIN</p>}
+                        {this.state.currentPKT < 100 && <p>PKT</p>}
+                    </section>
+                </section>
             </main>
         )
     }
